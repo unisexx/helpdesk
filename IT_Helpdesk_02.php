024d0b28065e68d280ba1e1a01c69d2ff52b75b6
unisexx <unisexx@hotmail.com>
2013-03-22 09:00:18 +0700

    IT_Helpdesk_02.php

--- IT_Helpdesk_02.php
+++ IT_Helpdesk_02.php
@@ -1,3 +1,23 @@
+<?
+  include "include/function.php";
+  include('include/adodb_connect.php');
+  $s_month = $_GET['s_month'];
+  $s_year = $_GET['s_year'];
+  
+  //ช่องทางการแจ้ง
+  $rs = $db->Execute("select * from request_lists where service = 'tel' and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+  $telcount = $rs->RecordCount();
+  
+  $rs = $db->Execute("select * from request_lists where service = 'email' and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+  $emailcount = $rs->RecordCount();
+  
+  $rs = $db->Execute("select * from request_lists where service = 'other' and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+  $othercount = $rs->RecordCount();
+?>
+<style>
+body{font-size:12px;}
+table.report td{text-align:center; padding:5px;}
+</style>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
@@ -11,279 +31,98 @@
     <td align="right">IT Helpdesk 02 </td>
   </tr>
   <tr>
-    <td align="center"><p>รายงานผลการดำเนินโครงการบริหารจัดการระบบสารสนเทศ (สป.พม.) ประจำปีงบประมาณ .................. <br />
-      เดือน ....................</p>
+    <td align="center"><p>รายงานผลการดำเนินโครงการบริหารจัดการระบบสารสนเทศ (สป.พม.) ประจำปีงบประมาณ <?php echo $s_year+543?> <br />
+      เดือน <?php echo GetMonthName('full',$s_month)?></p>
       <p>&nbsp;</p></td>
   </tr>
   <tr>
-    <td><table width="100%" border="1" cellpadding="0" cellspacing="0" bordercolor="#CCCCCC">
+    <td><table class="report" width="100%" border="1" cellpadding="0" cellspacing="0" bordercolor="#CCCCCC">
       <tr>
         <td rowspan="2" align="center">ลำดับ</td>
         <td rowspan="2" align="center">ชื่อระบบ</td>
         <td colspan="4" align="center">ประเภทการให้บริการ</td>
-        <td colspan="3" align="center">สถานะการดำเนินการ</td>
+        <td colspan="4" align="center">สถานะการดำเนินการ</td>
         <td rowspan="2" align="center">รวม</td>
       </tr>
       <tr>
-        <td align="center">งานปรับปรุง<br />
-          เพิ่มเติมโปรแกรม</td>
-        <td align="center">งานแก้ไข<br />
-          ข้อผิดพลาด</td>
-        <td align="center">งาน<br />
-          ร้องขอ</td>
-        <td align="center">งานให้<br />
-          คำแนะนำ</td>
-        <td align="center">กำลัง<br />
-          ดำเนินการ</td>
-        <td align="center">เรียบร้อย</td>
-        <td align="center">แจ้งกลับ</td>
-        </tr>
-      <tr>
-        <td align="center">1</td>
-        <td>ระบบงานบริหารราชการ</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบจัดทำคำของบประมาณ </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงานการคลัง </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบติดตามและประเมินผล </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงานตรวจราชการ </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงานเครือข่าย </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงาน พ.ร.บ.ส่งเสริมฯ </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงานบริหารกองทุน </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบบัญชีเงินกองทุน </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงานพัฒนาระบบบริหาร </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบงาน พ.ร.บ. คุ้มครองเด็ก </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบฐานข้อมูลกฏหมาย </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบสนับสนุนกระบวนการพัฒนากฏหมาย </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบสารบรรณอิเล็กทรอนิกส์ </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบทะเบียนพัสดุ </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>- ระบบพิมพ์สลิปเงินเดือน </td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">2</td>
-        <td>ระบบงานบริการทางสังคม</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">3</td>
-        <td>เว็บไซตืกระทรวง</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">4</td>
-        <td>พื้นที่จัดเก็บเว็บไซต์</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-      <tr>
-        <td align="center">&nbsp;</td>
-        <td>รวมทั้งหมด</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-        <td>&nbsp;</td>
-      </tr>
-    </table></td>
+        <?php 
+			$servicetypes = $db->GetAll("select * from servicetype order by id asc");
+			foreach($servicetypes as $servicetype):
+		?>
+			<th align="center"><?php echo $servicetype['service_name']?></th>
+		<?php endforeach;?>
+		<?php 
+			$problemstatuss = $db->GetAll("select * from problemstatus order by id asc");
+			foreach($problemstatuss as $problemstatus):
+		?>
+			<th align="center"><?php echo $problemstatus['name']?></th>
+		<?php endforeach;?>
+	  </tr>
+      <?php 
+		$systems = $db->GetAll("select * from system order by id asc");
+		foreach($systems as $key=>$system):
+	?>
+	<tr>
+        <td align="center"><?php echo $key+1?></td>
+        <td><?php echo $system['SystemName']?></td>
+        <?php 
+        	foreach($servicetypes as $servicetype):
+				$rs = $db->Execute("select * from request_lists where systemid = ".$system['ID']." and servicetype_id = ".$servicetype['id']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+				$servicecount = $rs->RecordCount();
+		?>
+			<td><?php echo $servicecount?></td>
+		<?php endforeach;?>
+		<?php 
+        	foreach($problemstatuss as $problemstatus):
+				$rs = $db->Execute("select * from request_lists where systemid = ".$system['ID']." and status = ".$problemstatus['id']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+				$statuscount = $rs->RecordCount();
+		?>
+			<td><?php echo $statuscount?></td>
+		<?php endforeach;?>
+		<?php
+			$rs = $db->Execute("select * from request_lists where systemid = ".$system['ID']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+			$totlecount = $rs->RecordCount();
+		?>
+        <td><?php echo $totlecount?></td>
+	</tr>
+	<?php endforeach;?>
+	<tr style="background: #FAF4FF; color:#65358f; font-weight: bold;">
+		<td></td>
+		<td>รวมทั้งหมด (รายการ)</td>
+		<?php 
+        	foreach($servicetypes as $servicetype):
+				$rs = $db->Execute("select * from request_lists where servicetype_id = ".$servicetype['id']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+				$servicecount_total = $rs->RecordCount();
+		?>
+			<td><?php echo $servicecount_total?></td>
+		<?php endforeach;?>
+		<?php 
+        	foreach($problemstatuss as $problemstatus):
+				$rs = $db->Execute("select * from request_lists where status = ".$problemstatus['id']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+				$statuscount_total = $rs->RecordCount();
+		?>
+			<td><?php echo $statuscount_total?></td>
+		<?php endforeach;?>
+		<?php
+			$rs = $db->Execute("select * from request_lists where month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+			$alltotlecount = $rs->RecordCount();
+		?>
+        <td><?php echo $alltotlecount?></td>
+	</tr>
+</table></td>
   </tr>
   <tr><td height="8"></td></tr>
   <tr>
-  	<td><font style="font-size:12px">ช่องทางการรับแจ้งทางโทรศัพท์ จำนวน .... รายการ ทางอีเมล์จำนวน ...... รายการ <br />
-  	  ประเภทปัญหา/สาเหตุ by app จำนวน .... รายการ By user จำนวน ......รายการ Hardware จำนวน ...... รายการ Network จำนวน ....... รายการ อื่น ๆ จำนวน ...... รายการ </font></td>
+  	<td><font style="font-size:12px">ช่องทางการรับแจ้งทางโทรศัพท์ จำนวน <?php echo $telcount?> รายการ ทางอีเมล์จำนวน <?php echo $emailcount?> รายการ  ทางอื่นๆจำนวน <?php echo $othercount?> รายการ<br />ประเภทปัญหา/สาเหตุ 
+  	<?php
+  		$problemtypes = $db->GetAll("select * from problemtype order by id asc");
+  		foreach($problemtypes as $problemtype):
+			$rs = $db->Execute("select * from request_lists where problemtype = ".$problemtype['ID']." and month(new_date)=".$s_month." and year(new_date)='".$s_year."'");
+			$problemcount = $rs->RecordCount();
+  	?>
+  		<?php echo $problemtype['ProblemName'].' จำนวน '.$problemcount.' รายการ, '?>
+  	<?php endforeach;?>
+	</font></td>
   </tr>
 </table>
 <p>&nbsp;</p>
